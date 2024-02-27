6b9fd8c3d59403ddd1d099cd7ad156e870ebcb5e
Brindusa22 Manu <brindusa.manu@gmail.com>
2024-02-27 11:46:45 +0000

    Correct typo

--- blog/models.py
+++ blog/models.py
@@ -22,6 +22,6 @@ class Comment(models.Model):
         Post, on_delete=models.CASCADE, related_name="comments")
     author= models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="commenter")
-    body = models.Textfield()
+    body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
