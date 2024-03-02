95ee489fc38ac76cb4ec9d16a923ea102dbf8788
Brindusa22 Manu <brindusa.manu@gmail.com>
2024-03-02 13:50:51 +0000

    Add tests to views for post requests

--- about/test_views.py
+++ about/test_views.py
@@ -13,6 +13,7 @@ class TestCollaborateForm(TestCase):
         self.about_content = About(title="About Me", content="This is about me")
         self.about_content.save()
 
+
     def render_about_page_with_form(self):
         """Verifies get request for about me containing a collaboration form"""
         response = self.client.get(reverse('about'))
@@ -20,3 +21,14 @@ class TestCollaborateForm(TestCase):
         self.assertIn(b"About Me", response.content)
         self.assertIn(b"This is about me", response.content)
         self.assertIsInstance(response.context["collaborate_form"], CollaborateForm)
+
+
+    def successful_request_submission(Self):
+        """Verifies the form was submitted correctly"""
+        collaborate_request = {name:"anna",
+                               email: "test@test.com",
+                               message:"This is my message"
+                               }
+        response = self.clent.post(reverse('about', collaborate_request))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Collaboration request received! I endeavour to respond within 2 working days.", response.content)
